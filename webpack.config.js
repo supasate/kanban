a0c5f1a8533d9b7c2872e4ffa679cae575a9801f
Supasate Choochaisri <supasate.c@gmail.com>
2015-12-03 02:47:43 +0700

    Hot Module Reloading

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 var path = require('path');
 var HtmlwebpackPlugin = require('html-webpack-plugin');
+var webpack = require('webpack');
 
 const PATHS = {
     app: path.join(__dirname, 'app'),
@@ -12,7 +13,17 @@ module.exports = {
         path: PATHS.build,
         filename: 'bundle.js'
     },
+    devServer: {
+        historyApiFallback: true,
+        hot: true,
+        inline: true,
+        progress: true,
+        stats: 'errors-only',
+        host: process.env.HOST,
+        port: process.env.PORT
+    },
     plugins: [
+        new webpack.HotModuleReplacementPlugin(),
         new HtmlwebpackPlugin({
             title: 'Kanban app'
         })
