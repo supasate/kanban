cfd57f7e777ad49c7cc660b93b1eb2b71767209e
Supasate Choochaisri <supasate.c@gmail.com>
2015-12-03 03:02:08 +0700

    Separate dev env

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,15 @@
 var path = require('path');
 var HtmlwebpackPlugin = require('html-webpack-plugin');
 var webpack = require('webpack');
+var merge = require('webpack-merge');
 
+const TARGET = process.env.npm_lifecycle_event;
 const PATHS = {
     app: path.join(__dirname, 'app'),
     build: path.join(__dirname, 'build')
 };
 
-module.exports = {
+var common = {
     entry: PATHS.app,
     output: {
         path: PATHS.build,
@@ -22,19 +24,27 @@ module.exports = {
             }
         ]
     },
-    devServer: {
-        historyApiFallback: true,
-        hot: true,
-        inline: true,
-        progress: true,
-        stats: 'errors-only',
-        host: process.env.HOST,
-        port: process.env.PORT
-    },
     plugins: [
-        new webpack.HotModuleReplacementPlugin(),
         new HtmlwebpackPlugin({
             title: 'Kanban app'
         })
     ]
 };
+
+if (TARGET === 'start' || !TARGET) {
+    module.exports = merge(common, {
+        devtool: 'eval-source-map',
+        devServer: {
+            historyApiFallback: true,
+            hot: true,
+            inline: true,
+            progress: true,
+            stats: 'errors-only',
+            host: process.env.HOST,
+            port: process.env.PORT
+        },
+        plugins: [
+            new webpack.HotModuleReplacementPlugin()
+        ]
+    });
+}
