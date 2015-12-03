d4c4ce6448028b55ce7c43552951957571ffae53
Supasate Choochaisri <supasate.c@gmail.com>
2015-12-04 01:36:02 +0700

    Show notes

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,6 +1,38 @@
-import React from 'react';
+import uuid from 'node-uuid';
+import React, {Component} from 'react';
 import Note from './Note.jsx';
 
-export default () => {
-    return <Note />;
+const notes = [
+    {
+        id: uuid.v4(),
+        task: 'Learn webpack'
+    },
+    {
+        id: uuid.v4(),
+        task: 'Learn React'
+    },
+    {
+        id: uuid.v4(),
+        task: 'Do laundry'
+    }
+];
+
+export default class App extends Component {
+    render () {
+        return (
+            <div>
+                <ul>
+                    {notes.map(this.renderNote)}
+                </ul>
+            </div>
+        );
+    }
+
+    renderNote (note) {
+        return (
+            <li key={note.id}>
+                <Note task={note.task} />
+            </li>
+        );
+    }
 };
