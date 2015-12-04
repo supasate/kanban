c048e3f2d4e9fa2e958ed1b3bccacc47cad8a5ef
Supasate Choochaisri <supasate.c@gmail.com>
2015-12-04 19:46:42 +0700

    Implement CRUD

--- app/stores/NoteStore.js
+++ app/stores/NoteStore.js
@@ -9,13 +9,48 @@ class NoteStore {
         this.notes = [];
     }
     create (note) {
+        const notes = this.notes;
 
+        note.id = uuid.v4();
+
+        this.setState({
+            notes: notes.concat(note)
+        });
     }
     update({id, task}) {
+        const notes = this.notes;
+        const noteIndex = this.findNote(id);
 
+        if (noteIndex < 0) {
+            return;
+        }
+
+        notes[noteIndex].task = task;
+
+        this.setState({
+            notes
+        });
     }
     delete(id) {
+        const notes = this.notes;
+        const noteIndex = this.findNote(id);
 
+        if (noteIndex < 0) {
+            return;
+        }
+        this.setState({
+            notes: notes.slice(0, noteIndex).concat(notes.slice(noteIndex + 1))
+        });
+    }
+    findNote (id) {
+        const notes = this.notes;
+        const noteIndex = notes.findIndex((note) => note.id === id);
+
+        if (noteIndex < 0) {
+            console.warn('Failed to find note', notes, id);
+        }
+
+        return noteIndex;
     }
 }
 
