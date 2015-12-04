aba474811749ab24d556ad0808624b314efed6c7
Supasate Choochaisri <supasate.c@gmail.com>
2015-12-04 19:58:03 +0700

    Gluing it all together

--- app/actions/NoteActions.js
+++ app/actions/NoteActions.js
@@ -1,3 +1,3 @@
 import alt from '../libs/alt';
 
-export defautl alt.generateActions('create', 'update', 'delete');
+export default alt.generateActions('create', 'update', 'delete');
--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,27 +1,22 @@
-import uuid from 'node-uuid';
 import React, {Component} from 'react';
 import Notes from './Notes.jsx';
+import NoteActions from '../actions/NoteActions';
+import NoteStore from '../stores/NoteStore';
 
 export default class App extends Component {
     constructor (props) {
         super(props);
 
-        this.state = {
-            notes: [
-                {
-                    id: uuid.v4(),
-                    task: 'Learn webpack'
-                },
-                {
-                    id: uuid.v4(),
-                    task: 'Learn React'
-                },
-                {
-                    id: uuid.v4(),
-                    task: 'Do laundry'
-                }
-            ]
-        };
+        this.state = NoteStore.getState();
+    }
+    componentDidMount () {
+        NoteStore.listen(this.storeChanged);
+    }
+    componentWillUnmount () {
+        NoteStore.unlisten(this.storeChanged);
+    }
+    storeChanged = (state) => {
+        this.setState(state);
     }
     render () {
         const notes = this.state.notes;
@@ -36,44 +31,12 @@ export default class App extends Component {
         );
     }
     addNote = () => {
-        this.setState({
-            notes: this.state.notes.concat([{
-                id: uuid.v4(),
-                task: 'New task'
-            }])
-        });
+        NoteActions.create({task: 'New Task'});
     }
     editNote = (noteId, task) => {
-        const notes = this.state.notes;
-        const noteIndex = this.findNote(noteId);
-
-        if (noteIndex < 0) {
-            return;
-        }
-
-        notes[noteIndex].task = task;
-
-        this.setState({notes});
-    }
-    findNote = (id) => {
-        const notes = this.state.notes;
-        const noteIndex = notes.findIndex((note) => note.id === id);
-
-        if (noteIndex < 0) {
-            console.warn('Failed to find note', notes, id);
-        }
-
-        return noteIndex;
+        NoteActions.update({id, task});
     }
     deleteNote = (id) => {
-        const notes = this.state.notes;
-        const noteIndex = notes.findIndex((note) => note.id === id);
-
-        if (noteIndex < 0) {
-            return;
-        }
-        this.setState({
-            notes: notes.slice(0,noteIndex).concat(notes.slice(noteIndex + 1))
-        });
+        NoteActions.delete(id);
     }
 };
