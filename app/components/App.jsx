af5ae8452a5f5b1e4ecc8f576433ad06ecdbe5bc
Supasate Choochaisri <supasate.c@gmail.com>
2015-12-05 10:03:49 +0700

    Add Lanes

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,48 +1,26 @@
 import AltContainer from 'alt-container';
 import React, {Component} from 'react';
-import Notes from './Notes.jsx';
-import NoteActions from '../actions/NoteActions';
-import NoteStore from '../stores/NoteStore';
+import Lanes from './Lanes.jsx';
+import LaneActions from '../actions/LaneActions';
+import LaneStore from '../stores/LaneStore';
 
 export default class App extends Component {
-    constructor (props) {
-        super(props);
-
-        this.state = NoteStore.getState();
-    }
-    componentDidMount () {
-        NoteStore.listen(this.storeChanged);
-    }
-    componentWillUnmount () {
-        NoteStore.unlisten(this.storeChanged);
-    }
-    storeChanged = (state) => {
-        this.setState(state);
-    }
     render () {
-        const notes = this.state.notes;
-
         return (
             <div>
-                <button className="add-note" onClick={this.addNote}>+</button>
+                <button className="add-lane" onClick={this.addItem}>+</button>
                 <AltContainer
-                    stores={[NoteStore]}
+                    stores={[LaneStore]}
                     inject={{
-                        items: () => NoteStore.getState().notes
+                        items: () => LaneStore.getState().lanes || []
                     }}
                 >
-                    <Notes onEdit={this.editNote} onDelete={this.deleteNote} />
+                    <Lanes />
                 </AltContainer>
             </div>
         );
     }
-    addNote = () => {
-        NoteActions.create({task: 'New Task'});
-    }
-    editNote = (id, task) => {
-        NoteActions.update({id, task});
-    }
-    deleteNote = (id) => {
-        NoteActions.delete(id);
+    addItem () {
+        LaneActions.create({name: 'New lane'});
     }
 };
---
+++ app/components/Lanes.jsx
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+import Lane from './Lane.jsx';
+
+export default class Lanes extends Component {
+    render () {
+        const lanes = this.props.items;
+        return (
+            <div className="lanes">
+                {lanes.map(this.renderLane)}
+            </div>
+        );
+    }
+    renderLane (lane) {
+        return <Lane className="lane" key={lane.id} lane={lane} />;
+    }
+}
