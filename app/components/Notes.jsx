d7fa5e3bd1aba23a2e3aa4d1490d6e91f59db4c0
Supasate Choochaisri <supasate.c@gmail.com>
2015-12-05 20:05:13 +0700

    Add onMove API

--- app/components/Notes.jsx
+++ app/components/Notes.jsx
@@ -11,12 +11,16 @@ export default class Notes extends Component {
 
     renderNote = (note) => {
         return (
-            <Note className="note" id={note.id} key={note.id}>
+            <Note className="note" onMove={this.onMoveNote}
+                id={note.id} key={note.id}>
                 <Editable
                     value={note.task}
                     onEdit={this.props.onEdit.bind(null, note.id)}
                     onDelete={this.props.onDelete.bind(null, note.id)} />
             </Note>
         );
+    }
+    onMoveNote({sourceId, targetId}) {
+        console.log('source', sourceId, 'target', targetId);
     }
 }
