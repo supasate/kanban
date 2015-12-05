7a0ee77ff933f7087c6f31dfea2e1d397361bf8b
Supasate Choochaisri <supasate.c@gmail.com>
2015-12-05 20:00:38 +0700

    Connect DropTarget

--- app/components/Note.jsx
+++ app/components/Note.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {DragSource} from  'react-dnd';
+import {DragSource, DropTarget} from  'react-dnd';
 import ItemTypes from '../constants/itemTypes';
 
 const noteSource = {
@@ -10,16 +10,28 @@ const noteSource = {
     }
 };
 
+const noteTarget = {
+    hover (targetProps, monitor) {
+        const sourceProps = monitor.getItem();
+
+        console.log('dragging note', sourceProps, targetProps);
+    }
+};
+
 @DragSource(ItemTypes.NOTE, noteSource, (connect) => ({
     connectDragSource: connect.dragSource()
+}))
+@DropTarget(ItemTypes.NOTE, noteTarget, (connect) => ({
+    connectDropTarget: connect.dropTarget()
 }))
 
 export default class Note extends React.Component {
     render () {
-        const {connectDragSource, id, onMove, ...props} = this.props;
+        const {connectDragSource, connectDropTarget,
+            id, onMove, ...props} = this.props;
 
-        return connectDragSource(
+        return connectDragSource(connectDropTarget(
             <li {...this.props}>{this.props.children}</li>
-        );
+        ));
     }
 }
