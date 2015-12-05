1cfae05cf89e30848fc6c423f81a7c1f6f426ddd
Supasate Choochaisri <supasate.c@gmail.com>
2015-12-05 20:29:56 +0700

    Able to drag and drop

--- app/stores/LaneStore.js
+++ app/stores/LaneStore.js
@@ -2,6 +2,7 @@ import uuid from 'node-uuid';
 import alt from '../libs/alt';
 import LaneActions from '../actions/LaneActions';
 import NoteStore from './NoteStore';
+import update from 'react/lib/update';
 
 class LaneStore {
     constructor () {
@@ -98,7 +99,32 @@ class LaneStore {
         return laneIndex;
     }
     move ({sourceId, targetId}) {
-        console.log('source', sourceId, 'target', targetId);
+        const lanes = this.lanes;
+        const sourceLane = lanes.filter((lane) => {
+            return lane.notes.indexOf(sourceId) >= 0;
+        })[0];
+        const targetLane = lanes.filter((lane) => {
+            return lane.notes.indexOf(targetId) >= 0;
+        })[0];
+        const sourceNoteIndex = sourceLane.notes.indexOf(sourceId);
+        const targetNoteIndex = targetLane.notes.indexOf(targetId);
+
+        if (sourceLane === targetLane) {
+            sourceLane.notes = update(sourceLane.notes, {
+                $splice: [
+                    [sourceNoteIndex, 1],
+                    [targetNoteIndex, 0, sourceId]
+                ]
+            });
+        } else {
+            // get rid of the source
+            sourceLane.notes.splice(sourceNoteIndex, 1);
+
+            // and move it to target
+            targetLane.notes.splice(targetNoteIndex, 0, sourceId);
+        }
+
+        this.setState({lanes});
     }
 }
 
