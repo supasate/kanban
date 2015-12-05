a0fa85c5d2ef10a03b17207035ab39706313f636
Supasate Choochaisri <supasate.c@gmail.com>
2015-12-05 20:34:06 +0700

    Hide dragging element in the same lane

--- app/components/Note.jsx
+++ app/components/Note.jsx
@@ -22,8 +22,9 @@ const noteTarget = {
     }
 };
 
-@DragSource(ItemTypes.NOTE, noteSource, (connect) => ({
-    connectDragSource: connect.dragSource()
+@DragSource(ItemTypes.NOTE, noteSource, (connect, monitor) => ({
+    connectDragSource: connect.dragSource(),
+    isDragging: monitor.isDragging() // map isDragging() state to isDragging prop
 }))
 @DropTarget(ItemTypes.NOTE, noteTarget, (connect) => ({
     connectDropTarget: connect.dropTarget()
@@ -31,11 +32,13 @@ const noteTarget = {
 
 export default class Note extends React.Component {
     render () {
-        const {connectDragSource, connectDropTarget,
+        const {connectDragSource, connectDropTarget, isDragging,
             id, onMove, ...props} = this.props;
 
         return connectDragSource(connectDropTarget(
-            <li {...this.props}>{this.props.children}</li>
+            <li style={{
+                    opacity: isDragging ? 0 : 1
+                }} {...this.props}>{this.props.children}</li>
         ));
     }
 }
