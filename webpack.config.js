853b84c0667bb2f703880e5a348502bb782c8c87
Supasate Choochaisri <supasate.c@gmail.com>
2015-12-06 10:48:13 +0700

    Add stats option

--- webpack.config.js
+++ webpack.config.js
@@ -64,7 +64,7 @@ if (TARGET === 'start' || !TARGET) {
         ],
     });
 }
-if (TARGET === 'build') {
+if (TARGET === 'build' || TARGET === 'stats') {
     module.exports = merge(common, {
         entry: {
             app: PATHS.app,
