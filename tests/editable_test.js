6d1fa467052b2e46ee339ba202e0ecbb5667655d
Supasate Choochaisri <supasate.c@gmail.com>
2015-12-08 13:28:33 +0700

    Allow deletion

--- tests/editable_test.js
+++ tests/editable_test.js
@@ -55,6 +55,20 @@ describe('Editable', () => {
         Simulate.blur(input);
 
         assert.equal(triggered, true);
+    });
 
+    it('allows deletion', () => {
+        let deleted = false;
+        const onDelete = () => {
+            deleted = true;
+        });
+        const component = renderIntoDocument(
+            <Editable value={'value'} onDelete={onDelete} />
+        );
+
+        let deleteComponent = findRenderedDOMComponentWithClass(component, 'delete');
+        Simulate.click(deleteComponent);
+
+        assert.equal(deleted, true);
     });
 })
