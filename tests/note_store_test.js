ae016b64aeca65c150d23515208c85a9903d99fe
Supasate Choochaisri <supasate.c@gmail.com>
2015-12-09 00:27:18 +0700

    Flush store each test and test NoteStore updates notes

--- tests/note_store_test.js
+++ tests/note_store_test.js
@@ -5,6 +5,10 @@ import NoteStore from 'app/stores/NoteStore';
 import alt from 'app/libs/alt';
 
 describe('NoteStore', () => {
+    beforeEach( () => {
+            alt.flush();
+    });
+
     it('creates notes', () => {
         const task = 'test';
 
@@ -14,5 +18,21 @@ describe('NoteStore', () => {
 
         assert.equal(state.notes.length, 1);
         assert.equal(state.notes[0].task, task);
-    })
+    });
+
+    it('updates notes', () => {
+        const task = 'test';
+        const updatedTask = 'test 2';
+
+        NoteActions.create({task});
+
+        const note = NoteStore.getState().notes[0];
+
+        NoteActions.update({...note, task: updatedTask});
+
+        const state = NoteStore.getState();
+
+        assert.equal(state.notes.length, 1);
+        assert.equal(state.notes[0].task, updatedTask);
+    });
 });
